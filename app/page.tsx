2b304e3cf10e3be15ecf1cff704660e2e4692362
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-02-15 21:45:30 +0800

    attempt to fix build fail #2.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,13 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Post from "./post";
 import { getPosts } from './actions';
 import Create from "./create";
 import SideMenu from "./sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function Home() {
-  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
   const [posts, setPosts] = useState<Record<string, any>[]>([]);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const handleResize = () => {
-        setScreenSize({ width: window.innerWidth, height: window.innerHeight });
-      };
-
-      handleResize(); // Set initial size
-      window.addEventListener("resize", handleResize);
-
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
-  }, []);
-
   useEffect(() => {
     const fetchPosts = async () => {
       const posts = await getPosts();
