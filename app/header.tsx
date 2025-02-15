29a29b6b1980a00e5b2e1b99bc94cc31643d398a
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-02-15 17:30:48 +0800

    add header component

---
+++ app/header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FiMenu } from "react-icons/fi";
+import { IoSearchOutline } from "react-icons/io5";
+
+interface HeaderProps {
+    toggleSidebar: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
+    return (
+        <header className="w-full flex items-center justify-between p-4 bg-blue-300">
+            <div className="header__icon" onClick={toggleSidebar}>
+                <FiMenu />
+            </div>
+            <div className="header__title text-center flex-grow">
+                <h1>Title Name</h1>
+            </div>
+            <div className="header__icon">
+                <IoSearchOutline />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
