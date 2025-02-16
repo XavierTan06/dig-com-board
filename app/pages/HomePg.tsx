ef62c2c76b3457dbd11d7e3c8e7345645c6cfc8f
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-02-16 19:37:03 +0800

    start separation of concern by creating homepg page

--- app/pages/HomePg.tsx
+++ app/pages/HomePg.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Post from "./components/post";
-import Header from "./components/header";
-import { getPosts } from './actions';
-import Create from "./pages/create";
-import SideMenu from "./components/sidebar";
+import Post from "../components/post";
+import Header from "../components/header";
+import { getPosts } from '../actions';
+import Create from "./create";
+import SideMenu from "../components/sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePg from "./pages/HomePg";
 
-export default function App() {
+export default function HomePg() {
   const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
   const [posts, setPosts] = useState<Record<string, any>[]>([]);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -53,14 +52,20 @@ export default function App() {
   }
 
   return (
-    <BrowserRouter>
-      <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-5 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <SideMenu />
-        <Routes>
-          <Route path="/tester" element={<Create />} />
-          <Route path="/" element={<HomePg />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+    <><div className="posts-container grid gap-8 sm:grid-cols-1 lg:grid-cols-1 mt-8">
+          {posts.map((post, index) => (
+              <Post
+                  key={index}
+                  title={post.post_title}
+                  text={post.post_text}
+                  like_count={post.like_count}
+                  reply_count={post.reply_count}
+                  date={post.date} />
+          ))}
+      </div><main className="flex flex-col gap-8 items-center sm:items-start mt-8">
+              <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+                  <li>Save and see your changes instantly. Your screen size is {screenSize.width} by {screenSize.height}!</li>
+              </ol>
+          </main></>
+  )
 }
