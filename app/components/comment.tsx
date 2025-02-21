f3beda36ca7d6f48f6e51fc34717cb927088c9fc
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-02-21 21:03:06 +0800

    implement comment component

---
+++ app/components/comment.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { FaThumbsUp } from "react-icons/fa";
+import { incrementLike } from '../actions';
+import exp from "constants";
+
+interface CommentProps {
+  text: string;
+  like_count: number;
+  date: string;
+  id: string;
+}
+
+function Comment({ text, like_count, date, id }: CommentProps) {
+    const [likes, setLikes] = useState(like_count);
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+    const handleLikeClick = async () => {
+        if (isButtonDisabled) return;
+        setLikes(likes + 1);
+        setIsButtonDisabled(true);
+        await incrementLike(id, true);
+        alert('Liked!');
+        setTimeout(() => {
+            setIsButtonDisabled(false);
+        }, 10000);
+    };    
+
+        return (
+            <div className="bg-gray-200 p-0 rounded-lg mb-1 border border-gray-300 md:p-6 lg:p-8">
+                <p className="text-blue-500 mt-2 md:mt-4 lg:mt-6">{text}</p>
+                <div className="flex items-center mt-3 md:mt-4 lg:mt-6">
+                    <div className="flex items-center text-blue-500 mr-6">
+                        <FaThumbsUp 
+                            className={`mr-2 cursor-pointer ${isButtonDisabled ? 'opacity-50' : ''} z-10`} 
+                            onClick={handleLikeClick} 
+                        /> 
+                        {likes}
+                    </div>
+                </div>
+                <small className="text-blue-400 mt-2 block md:mt-4 lg:mt-6">{date}</small>
+            </div>
+        );
+}
+
+export default Comment;
