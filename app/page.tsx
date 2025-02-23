775c1b33bf763c40b966fc92472be9403f53e5b0
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-02-23 22:41:40 +0800

    refactor home page by adding it to app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,60 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Create from "./create/page";
-import SideMenu from "../components/sidebar";
-import HomePg from "./pages/HomePg";
-import PostThreadPg from "./posts/PostPg";
+import Post from "../components/post";
+import { getPosts } from './actions';
 import { useRouter, usePathname } from "next/navigation";
+
+function HomePage() {
+  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
+  const [posts, setPosts] = useState<Record<string, any>[]>([]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    handleResize(); // Set initial size
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const posts = await getPosts();
+      const sortedPosts = posts.sort((a, b) => new Date(b.post_date).getTime() - new Date(a.post_date).getTime());
+      setPosts(sortedPosts);
+      console.log(posts);
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <><div className="posts-container grid gap-0 sm:grid-cols-1 lg:grid-cols-1 mt-0">
+          {posts.map((post, index) => (
+              <Post
+                  key={index}
+                  title={post.post_title}
+                  text={post.post_text}
+                  like_count={post.like_count}
+                  reply_count={post.reply_count}
+                  date={new Date(post.post_date).toLocaleString()}
+                  id={post.post_id} />
+          ))}
+      </div><main className="flex flex-col gap-8 items-center sm:items-start mt-8">
+              <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+                  <li>Save and see your changes instantly. Your screen size is {screenSize.width} by {screenSize.height}!</li>
+              </ol>
+          </main></>
+  )
+}
 
 export default function App() {
   const [isClient, setIsClient] = useState(false); // State to check if the component is rendered on the client
-  const router = useRouter();
-  const pathname = usePathname();
 
   useEffect(() => {
     setIsClient(true); // Set to true once the component is mounted on the client side
@@ -23,7 +67,7 @@ export default function App() {
 
   return (
     <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-5 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <HomePg />
+      <HomePage />
     </div>
   );
 }
