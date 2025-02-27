52a89556d1782d6ba10de92f52006e55ee885a44
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-02-27 20:10:14 +0800

    implement sanitisation and setting of inner html

--- components/post.tsx
+++ components/post.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FaThumbsUp, FaHeart } from 'react-icons/fa';
 import { incrementLike } from '../app/actions';
+import DOMPurify from 'dompurify';
 
 interface PostProps {
     title: string;
@@ -26,10 +27,16 @@ const Post: React.FC<PostProps> = ({ title, text, like_count, reply_count, date,
         }, 10000);
     };
 
+    // Sanitize the HTML text content before rendering
+    const sanitizedText = DOMPurify.sanitize(text);
+
     return (
         <div className="bg-gray-200 p-0 rounded-lg mb-1 border border-gray-300 md:p-6 lg:p-8">
             <h2 className="text-blue-600 text-xl font-semibold md:text-2xl lg:text-3xl">{title}</h2>
-            <p className="text-blue-500 mt-2 md:mt-4 lg:mt-6">{text}</p>
+            <p
+                className="text-blue-500 mt-2 md:mt-4 lg:mt-6"
+                dangerouslySetInnerHTML={{ __html: sanitizedText }}
+            />
             <div className="flex items-center mt-3 md:mt-4 lg:mt-6">
                 <div className="flex items-center text-blue-500 mr-6">
                     <FaThumbsUp 
