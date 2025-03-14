7e091f51acd2786020bb2c756ced250354c36d42
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-03-14 21:31:54 +0800

    add nickname to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FiMenu } from "react-icons/fi";
 import { IoSearchOutline } from "react-icons/io5";
+import { NicknameContext } from '../context/context';
 
 interface HeaderProps {
     toggleSidebar: () => void;
 }
 
 const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
+    const nicknameContext = useContext(NicknameContext);
+    const nickname = "Browsing as: " + (nicknameContext?.nickname ?? "Anonymous");
+
     return (
         <header className="w-full flex items-center justify-between p-4 bg-blue-300">
             <div className="header__icon" onClick={toggleSidebar}>
@@ -16,7 +20,7 @@ const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
                 <h1>Community Board</h1>
             </div>
             <div className="header__icon">
-                <IoSearchOutline />
+                {nickname}
             </div>
         </header>
     );
