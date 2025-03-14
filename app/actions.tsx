d7cbc3c725004129c391e4dc3b403e0658b15d84
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-03-14 21:51:25 +0800

    modify create, getpost to include author

--- app/actions.tsx
+++ app/actions.tsx
@@ -4,15 +4,15 @@ import { neon } from '@neondatabase/serverless';
 import { v4 as uuidv4 } from 'uuid';
 import { Event } from '@/app/calendar/page';
 
-export async function create(formData: FormData) {
+export async function create(formData: FormData, author: string) {
   const sql = neon(`${process.env.DATABASE_URL}`);
   const postId = uuidv4();
   const postTitle = formData.get('post_title');
   const postText = formData.get('post_text');
   const postDate = new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Singapore' })); // GMT+8
   await sql(
-    'INSERT INTO test_post (post_id, post_title, post_text, like_count, reply_count, post_date) VALUES ($1, $2, $3, $4, $5, $6)',
-    [postId, postTitle, postText, 0, 0, postDate]
+    'INSERT INTO test_post (post_id, post_title, post_text, like_count, reply_count, post_date, author) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+    [postId, postTitle, postText, 0, 0, postDate, author]
   );
   return postId;
 }
@@ -31,10 +31,10 @@ export async function reply(formData: FormData, parentPost: string) {
 export async function getPosts(id: null | string = null) {
   const sql = neon(`${process.env.DATABASE_URL}`);
   if (id) {
-    const result = await sql('SELECT post_title, post_text, like_count, reply_count, post_date, post_id FROM test_post WHERE post_id = $1', [id]);
+    const result = await sql('SELECT post_title, post_text, like_count, reply_count, post_date, post_id, author FROM test_post WHERE post_id = $1', [id]);
     return result;
   }
-  const result = await sql('SELECT post_title, post_text, like_count, reply_count, post_date, post_id FROM test_post');
+  const result = await sql('SELECT post_title, post_text, like_count, reply_count, post_date, post_id, author FROM test_post');
   console.log(result);
   return result;
 }
