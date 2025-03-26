76b923219ec04511fc711b5f6d31027a08305050
XavierTan06 <tangentxavier@gmail.com>
2025-03-26 13:52:38 +0800

    add bg image to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,11 @@ function LandingPage() {
   const [showMore, setShowMore] = useState(false); // State to toggle "Show More" content
 
   return (
-    <div className="flex flex-col items-center bg-gray-100 p-8 z-0">
+    <div className="flex flex-col items-center p-8 z-0">
       <h1 className="text-4xl font-bold text-gray-900 mb-2">Healthy Bedok North!</h1>
       <p className="text-lg text-gray-600 mb-6">Improving health of residents and patients</p>
       {/* Image Container - will need modification eventually when pics furnished */}
       <div className="grid grid-cols-1 gap-4 mb-6">
-        <img src="/250221_wetlands_v1.png" alt="Image 1" className="w-60 h-40 object-cover rounded-lg shadow-md" />
         <img src="/250226_sculpturepark.png" alt="Image 2" className="w-60 h-40 object-cover rounded-lg shadow-md" />
       </div>
 
@@ -72,7 +71,14 @@ export default function App() {
   }
 
   return (
-    <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-5">
+    <div
+      className="flex flex-col items-center min-h-screen p-8 pb-20 gap-5"
+      style={{
+      backgroundImage: "url(/250221_wetlands_v1.png)",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      }}
+    >
       <LandingPage />
     </div>
   );
