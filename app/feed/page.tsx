ce7d19aeaf2d0a94028464979f723a65ae546105
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-03-28 21:35:29 +0800

    remove extra divs

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -12,12 +12,10 @@ export default async function HomePage() {
   );
 
   return (
-    <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-5 mt-4">
-      <div className="posts-container grid gap-0 sm:grid-cols-1 lg:grid-cols-1 mt-0">
+    <div className="p-8">
         <Suspense fallback={<div>Loading...</div>} >
           <PostList posts={sortedPosts} />
         </Suspense>
-      </div>
     </div>
   );
 }
