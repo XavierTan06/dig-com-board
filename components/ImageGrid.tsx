48711d95fb978ac6c959b31686cdecd9dc89a21f
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-03-28 19:03:00 +0800

    display images in horizontal scrolling view

--- components/ImageGrid.tsx
+++ components/ImageGrid.tsx
@@ -3,14 +3,13 @@ import Image from 'next/image';
 
 export default function ImageGrid() {
   const memoizedContent = useMemo(() => (
-    <div className="mt-4 text-white grid grid-cols-1 gap-4 justify-items-center">
-      <p className="text-center">Here is some additional content that was previously hidden.</p>
+    <div className="text-white flex overflow-x-auto space-x-4 pt-4">
       <Image
         src="/gallery/250207_Wellness Park Plan_Labeled.webp"
         alt="Image 2"
         width={240}
         height={160}
-        className="object-cover rounded-lg shadow-md"
+        className="object-cover rounded-lg shadow-md flex-shrink-0"
         loading="lazy"
       />
       <Image
@@ -18,7 +17,7 @@ export default function ImageGrid() {
         alt="Image 3"
         width={240}
         height={160}
-        className="object-cover rounded-lg shadow-md"
+        className="object-cover rounded-lg shadow-md flex-shrink-0"
         loading="lazy"
       />
     </div>
