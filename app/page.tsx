a77a882d3f194694b80cceb4535e7a74780ef12a
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-03-28 19:07:43 +0800

    fix quotation

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ function LandingPage() {
           {showMore ? "Show Less" : "Show More"}
         </button>
         <div className={showMore ? "block" : "hidden"}>
-        <p className="text-center text-white pt-4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        <p className="text-center text-white pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <Suspense fallback={<div>Loading images...</div>}>
             <ImageGrid />
           </Suspense>
