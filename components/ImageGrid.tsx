adf43f820b7961b6012a07c321cb6d4692ed136c
XavierTan06 <tangentxavier@gmail.com>
2025-03-29 09:42:50 +0800

    map images

--- components/ImageGrid.tsx
+++ components/ImageGrid.tsx
@@ -1,25 +1,28 @@
 import { useMemo } from 'react';
 import Image from 'next/image';
 
+const images = [
+  "/gallery/Wellness Park Design - With big labels-14.webp",
+  "/gallery/250324_Sculpture Park_VF.webp",
+  "/gallery/250326_Bamboo Forest Final.webp",
+  "/gallery/250326_Meditative Pond.webp",
+  "/gallery/Social Kitchen.webp"
+];
+
 export default function ImageGrid() {
   const memoizedContent = useMemo(() => (
     <div className="text-white flex overflow-x-auto space-x-4 pt-4">
+      {images.map((src, index) => (
       <Image
-        src="/gallery/250207_Wellness Park Plan_Labeled.webp"
-        alt="Image 2"
+        key={index}
+        src={src}
+        alt={`Image ${index + 1}`}
         width={240}
         height={160}
         className="object-cover rounded-lg shadow-md flex-shrink-0"
         loading="lazy"
       />
-      <Image
-        src="/gallery/250226_therapeutic garden_motion blur.webp"
-        alt="Image 3"
-        width={240}
-        height={160}
-        className="object-cover rounded-lg shadow-md flex-shrink-0"
-        loading="lazy"
-      />
+      ))}
     </div>
   ), []); // Memoize so it doesn't re-render on state change
 
