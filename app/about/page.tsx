31dd3be737a7ce7fd9b03d1089566c8a20fa50e2
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-04-01 17:55:11 +0800

    placeholder functions for About page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+function Images() {
+  return <div>Images</div>;
+}
+
+function Writeup() {
+  return <div>Writeup</div>;
+}
+
+export default function AboutPage() {
+    return <div>
+        <Images />
+    </div>
+}
