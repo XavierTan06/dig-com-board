0556493cf76276be79b7143be25b5d19b27def26
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-04-01 18:53:32 +0800

    complete about prototype

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,26 +1,33 @@
 import ImageCard from "@/components/ImageCard";
 
+const imagesList = {
+    "/gallery/ParkDesignBigLabels.webp": "caption 1",
+    "/gallery/SculptureParkVF.webp": "caption 2",
+    "/gallery/BambooForest.webp": "caption 3",
+    "/gallery/MeditativePond.webp": "caption 4",
+    "/gallery/SocialKitchen.webp": "caption 5"
+};
+
 function Images() {
   return <div>
-    <h1 className="text-4xl font-bold text-black mb-2">Images</h1>
-    <div className="flex flex-wrap justify-center">
-      <ImageCard src="/gallery/250207_Wellness Park Plan_Labeled.webp" alt="Image 1" caption="Lorem ipsum dolor sit amet" />
-      <ImageCard src="/images/image2.jpg" alt="Image 2" caption="Image 2" />
-      <ImageCard src="/images/image3.jpg" alt="Image 3" caption="Image 3" />
-      <ImageCard src="/images/image4.jpg" alt="Image 4" caption="Image 4" />
+    <h1 className="text-center text-4xl font-bold text-black mb-2">Images</h1>
+    <div className="flex flex-col justify-center">
+      {Object.entries(imagesList).map(([src, caption]) => (
+        <ImageCard key={src} src={src} alt={caption} caption={caption} />
+      ))}
     </div>
   </div>;
 }
 
 function Writeup() {
-  return <div>
-    <h1 className="text-4xl font-bold text-black mb-2">About Our Project</h1>
+  return <div className="pt-4">
+    <h1 className="text-center text-4xl font-bold text-black mb-2">About Our Project</h1>
     <p className="text-center text-gray-900 pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>;
 }
 
 export default function AboutPage() {
-    return <div className="flex flex-col items-center">
+    return <div className="flex flex-col items-center gap-4 z-10">
         <Writeup />
         <Images />
     </div>
--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -4,7 +4,7 @@ const ImageCard = ({ src, alt, caption }: { src: string; alt: string; caption: s
     return (
         <div style={{ textAlign: 'center', margin: '1rem' }}>
             {/* Next.js Image */}
-            <Image src={src} alt={alt} width={300} height={200} style={{ borderRadius: '8px' }} />
+            <Image src={src} alt={alt} width={240} height={160} style={{ borderRadius: '8px' }} loading= 'lazy' />
             {/* Caption */}
             <p style={{ marginTop: '0.5rem', fontSize: '0.9rem', color: '#555' }}>{caption}</p>
         </div>
