f6602688a0ba7f615e6ef6055f2687b59e03cffc
XavierTan06 <tangentxavier@gmail.com>
2025-04-04 16:28:57 +0800

    modify header to use nextjs link

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { FiMenu } from "react-icons/fi";
 import { NicknameContext } from '../context/context';
+import Link from 'next/link';
 
 interface HeaderProps {
     toggleSidebar: () => void;
@@ -18,11 +19,10 @@ const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
             <div className="justify-self-start cursor-pointer" onClick={toggleSidebar}>
                 <FiMenu />
             </div>
-            <h1 
-                className="justify-self-center cursor-pointer font-bold" 
-                onClick={() => window.location.href = '/'}
-            >
-                Community Board
+            <h1 className="justify-self-center cursor-pointer font-bold">
+                <Link href="/">
+                    Community Board
+                </Link>
             </h1>
             <span className="justify-self-end text-[14px]">{nickname}</span>
         </header>
