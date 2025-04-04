100af984f23017981eed8a263b97000fb687c0dd
XavierTan06 <tangentxavier@gmail.com>
2025-04-04 16:27:40 +0800

    add link to create post

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -3,6 +3,18 @@
 import { Suspense } from "react";
 import { getPosts } from "../actions";
 import PostList from "@/components/postlist";
+import Link from "next/link";
+
+function CreateButton() {
+  return (
+    <Link
+      href="/create"
+      className="p-2 bg-gray-800 text-white rounded-lg shadow-md hover:bg-gray-700 transition z-0"
+    >
+      Create a Post
+    </Link>
+  );
+}
 
 export default async function HomePage() {
   // Fetch posts on the server side
@@ -12,7 +24,8 @@ export default async function HomePage() {
   );
 
   return (
-    <div className="p-8">
+    <div className="flex flex-col p-8 items-center gap-5 ">
+      <CreateButton/>
         <Suspense fallback={<div>Loading...</div>} >
           <PostList posts={sortedPosts} />
         </Suspense>
